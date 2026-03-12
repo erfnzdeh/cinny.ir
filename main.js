d1cf5944e5fa985a676fa17cfce3ed48884e649a
Pouri <erfnzdeh@gmail.com>
2026-03-12 23:11:55 +0330

    Add Persian localization
    Update index.html for Persian localization and replace icons; add Vazirmatn font family in CSS; modify theme selection text; remove unused JavaScript functions; adjust layout for RTL support.

--- main.js
+++ main.js
@@ -16,22 +16,22 @@ function changeTheme(that) {
   switch (themeName) {
     case 'silver-theme':
       previewImage.src = 'assets/preview-silver.png';
-      themeStatus.textContent = 'Selected: Silver'
+      themeStatus.textContent = 'انتخاب شده: Silver';
       previewImgSrc = 'assets/preview-silver.png';
       break;
     case 'dark-theme':
       previewImage.src = 'assets/preview-dark.png';
-      themeStatus.textContent = 'Selected: Dark'
+      themeStatus.textContent = 'انتخاب شده: Dark';
       previewImgSrc = 'assets/preview-dark.png';
       break;
     case 'butter-theme':
       previewImage.src = 'assets/preview-butter.png';
-      themeStatus.textContent = 'Selected: Butter'
+      themeStatus.textContent = 'انتخاب شده: Butter';
       previewImgSrc = 'assets/preview-butter.png';
       break;
     default:
       previewImage.src = 'assets/preview-light.png';
-      themeStatus.textContent = 'Selected: Light'
+      themeStatus.textContent = 'انتخاب شده: Light';
       previewImgSrc = 'assets/preview-light.png';
   }
 
@@ -40,43 +40,3 @@ function changeTheme(that) {
     featImg.src = previewImgSrc;
   }
 }
-
-function getSupporterProfile(supporter) {
-  let profilePic = null
-  if (typeof supporter.image === 'string') {
-    profilePic = document.createElement('img');
-    profilePic.src = supporter.image;
-  } else {
-    profilePic = document.createElement('div');
-    profilePic.classList.add('text-s1')
-    profilePic.textContent = supporter.name.slice(0, 2);
-  }
-  profilePic.classList.add('supporterProfile');
-  const link = document.createElement('a');
-  link.href = supporter.profile;
-  link.target = '_blank';
-  link.append(profilePic);
-  link.title = supporter.name;
-  return link;
-}
-
-async function loadSupport() {
-  const supportOrg = document.getElementById('supportOrg');
-  const supportInd = document.getElementById('supportInd');
-  const ORGURL = 'https://opencollective.com/cinny/members/organizations.json';
-  const INDURL = 'https://opencollective.com/cinny/members/users.json';
-
-  const orgs= await (await fetch(ORGURL, { method: 'GET' })).json();
-  orgs.forEach((org) => {
-    if (org.totalAmountDonated === 0) return;
-    supportOrg.append(getSupporterProfile(org));
-  });
-
-  const inds = await (await fetch(INDURL, { method: 'GET' })).json();
-  inds.forEach((ind) => {
-    if (ind.totalAmountDonated === 0) return;
-    supportInd.append(getSupporterProfile(ind));
-  });
-}
-
-loadSupport();
